6f65d81741c1a77db748a0bc0c690d3fef2e8414
Dawid Grzegorczyk <dawid.grzegorczyk@gmail.com>
2026-05-15 19:29:45 +0200

    Add second inventer modbus data
    Change-Id: I3bf4f119ad577ba2c4b9a0520363881c548f1d7d

--- modbus-debug.py
+++ modbus-debug.py
@@ -1,19 +1,23 @@
 from pymodbus.client import ModbusTcpClient
 
 # Ustawienia połączenia
-client = ModbusTcpClient("192.168.1.50", port=1502)
+client = ModbusTcpClient("192.168.1.20", port=1502)
 client.connect()
 
-# Wysłanie requestu i odczyt rejestrów
-address = 40001
-count = 10
-result = client.read_holding_registers(address - 40001, count, unit=1)
+try:
+    # Wysłanie requestu i odczyt rejestrów
+    address = 40001
+    count = 10
+    result = client.read_holding_registers(address=address - 40001, count=count, device_id=126)
 
-# Wyświetlenie wyników
-if result.isError():
-    print(f"Błąd: {result}")
-else:
-    print(result.registers)
+
+    # Wyświetlenie wyników
+    if result.isError():
+        print(f"Błąd: {result}")
+    else:
+        print(result.registers)
+except Exception as exc:
+    print(exc)
 
 # Zamknięcie połączenia
 client.close()
